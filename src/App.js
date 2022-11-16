c56666577a8ff3e4962d9c933944faf10cfee7b2
osmergit <osmer1976@gmail.com>
2022-11-15 19:37:01 -0500

    Proyecto Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+//mis componentes
+import { CompShowUsers } from './deportes/Mostrarusuarios.jsx'
+import { CompCreateUser } from './deportes/Crearusuarios.jsx'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     </header>
+      <BrowserRouter>
+
+                      <Routes>
+                              <Route path= '/users' element= { <CompShowUsers />} />
+                              <Route path= '/create' element= { <CompCreateUser />} />
+
+                      </Routes>
+      
+      </BrowserRouter>
+
     </div>
   );
 }
